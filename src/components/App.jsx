25b670171c9e1c30637ea23b1c27f968352fe627
OleksiiPanaioti <cavedzgn@gmail.com>
2023-04-12 13:21:50 +0300

    finished markup of t1 and t2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Profile } from "./Profile";
+
+import { Statistics } from "./Statistics";
+
+import user from "../user.json";
+
+import data from "../data.json";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template 123
+    <div>
+      <div><Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats} /></div>
+      
+      <div>
+        <Statistics title="Upload stats" stats={data} />
+        {/* <Statistics stats={data} /> */}
+      </div>
+      
     </div>
   );
+
 };
+
