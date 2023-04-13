9f89ca73303a469ebe09a28ad3cee38ed099b6f0
OleksiiPanaioti <cavedzgn@gmail.com>
2023-04-13 08:51:15 +0300

    3-4 task added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,44 @@
-import { Profile } from "./Profile";
+import { Profile } from './Profile';
 
-import { Statistics } from "./Statistics";
+import { Statistics } from './Statistics';
 
-import user from "../user.json";
+import { Friendlist } from './Friendlist';
 
-import data from "../data.json";
+import { TransactionHistory } from './Transactions';
+
+import user from '../user.json';
+
+import data from '../data.json';
+
+import friends from '../friends.json';
+
+import transactions from '../transactions.json';
 
 export const App = () => {
   return (
     <div>
-      <div><Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats} /></div>
-      
+      <div>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+
       <div>
         <Statistics title="Upload stats" stats={data} />
-        {/* <Statistics stats={data} /> */}
+        <Statistics stats={data} />
       </div>
-      
+
+      <div>
+        <Friendlist friends={friends} />
+      </div>
+
+      <div>
+        <TransactionHistory item={transactions} />
+      </div>
     </div>
   );
-
 };
-
